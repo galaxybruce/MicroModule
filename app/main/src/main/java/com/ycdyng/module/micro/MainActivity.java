fe67d74656be650ce576eef3265da3a8097b3c09
bruce <brucezhanggood@gmail.com>
2018-04-26 15:36:52 +0800

    app/build.gradle中添加java.src

--- app/main/src/main/java/com/ycdyng/module/micro/MainActivity.java
+++ app/main/src/main/java/com/ycdyng/module/micro/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ycdyng.module.micro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.hello).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/p_home/src/main/java/com.ycdyng.module.micro/HomeActivity.java
+++ app/p_home/src/main/java/com.ycdyng.module.micro/HomeActivity.java
@@ -1,6 +1,20 @@
 package com.ycdyng.module.micro;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        findViewById(R.id.hello).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
